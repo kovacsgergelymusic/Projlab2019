932015adb7d434ce32e86fa664d1526d17e1c94a
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-20 15:18:32 +0100

    Update Thing.java

--- src/Thing.java
+++ src/Thing.java
@@ -1,16 +1,5 @@
-import java.util.ArrayList;
-import java.util.Hashtable;
-public class Tile {
-	private int sides;
-	private Hashtable<Integer,Tile> neighbors;
-	private Animal animal;
-	private Thing thing;
-	public boolean AcceptPanda(Panda p) {return true;}
-	public boolean AcceptOrangutan(Orangutan o) {return true;}
-	public void RemoveAnimal(Animal a) {}
-	public void JumpedOn() {}
-	public Tile GetNeighbor(int side) {return null;}
-	public int GetSides() {return sides;}
-	public int CompareTile(Tile t) {return 0;}
-	public void SetAnimal (Animal a) {animal=a;}
+public abstract class Thing implements Steppable, Observable{
+	private Tile tile;
+	private ArrayList<Observer> observers;
 }
+
