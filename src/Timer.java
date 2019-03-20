3bdfd9c4be74bba0b803dc72e3b6e1f045f2d45c
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-20 15:15:37 +0100

    Update Timer.java

--- src/Timer.java
+++ src/Timer.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 public class Timer {
 	private ArrayList<Steppable> steppables;
-	public void Tick() {}
-	public void RemoveSteppable(Steppable s) {}
+	public void Tick() {
+		for (Steppable s:steppables){
+			s.Step();
+		}
+	}
+	public void RemoveSteppable(Steppable s) {
+		steppables.remove(s);
+	}
 }
