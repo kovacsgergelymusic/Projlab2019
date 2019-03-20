221476af4f0c14a3c249e4de21916f35d1c2dec4
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-20 15:13:50 +0100

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,16 +1,31 @@
-
 public abstract class Animal {
-	private Animal pulled;
-	private Tile tile;
+	protected Panda pulled;
+	protected Tile tile;
 	
-	public void Fall() {}
-	public void Destroy(){}
+	public void Fall() {
+		Destroy();
+	}
+	public void Destroy(){
+		tile.RemoveAnimal(this);
+		Game.getInstance().DeleteAnimal(this);
+		if (pulled!=null) {
+			Disband();
+		}
+	}
 	public void Move(int side){}
 	public boolean CollideWithPanda(Panda p){return true;}
 	public boolean CollideWithOrangutan(Orangutan o){return true;}
-	public void Disband(){}
-	public Tile GetTile(){return tile;}
-	public Animal GetPulled(){return pulled;}
-	public void SetPulled(Animal a){pulled=a;}
+	public void Disband(){
+		pulled.Disband();
+	}
+	public Tile GetTile(){
+		return tile;
+		}
+	public Animal GetPulled(){
+		return pulled;
+		}
+	public void SetPulled(Panda p){
+		pulled=p;
+		}
 	
 }
