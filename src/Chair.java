a77ea65dab7df31aee238671444432ade64f85f9
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-21 14:31:38 +0100

    Update Chair.java

--- src/Chair.java
+++ src/Chair.java
@@ -1,9 +1,19 @@
-
 public class Chair extends Thing {
 	private boolean occupied;
-	public void Step() {}
-	public void Notify() {}
-	public boolean GetOccupied() {return occupied;}
-	public void Detach(Observer o) {}
-
+	public void Step() {
+		if (!occupied) {
+			Notify();
+		}
+	}
+	public void Notify() {
+		for (Observer o:observers) {
+			o.Update(this);
+		}
+	}
+	public boolean GetOccupied() {
+		return occupied;
+		}
+	public void Detach(Observer o) {
+		observers.remove(o);
+	}
 }
