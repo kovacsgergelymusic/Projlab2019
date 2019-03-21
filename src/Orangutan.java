7b51648ece76a25c49a337b68a053969f4651292
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-21 14:32:31 +0100

    Update Orangutan.java

--- src/Orangutan.java
+++ src/Orangutan.java
@@ -1,8 +1,24 @@
-
 public class Orangutan extends Animal {
 	private int points;
-	public void Addpoint() {points++;}
-	public void Move(int side) {}
-	public boolean CollideWithPanda(Panda p){return true;}
-	public boolean CollideWithOrangutan(Orangutan o){return true;}
+	public void Addpoint() {
+		points++;}
+	public void Move(int side) {
+		Tile t2=tile.GetNeighbor(side);
+		if (t2.AcceptOrangutan(this)) {	
+			tile.RemoveAnimal(this);
+			t2.SetAnimal(this);
+			if (pulled!=null) {
+				Tile t3=pulled.GetTile();
+				int a=tile.CompareTile(t3);
+				pulled.Move(a);
+			}
+		}
+	}
+	public boolean CollideWithPanda(Panda p){
+			p.CaughtbyOrangutan(this);
+			return false;
+		}
+	public boolean CollideWithOrangutan(Orangutan o){
+		return false;
+	}
 }
