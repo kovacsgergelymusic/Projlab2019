6d2198d1a822e8ac7c37379b4ce988bd5fdf57da
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-21 20:07:05 +0100

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -3,14 +3,38 @@ import java.util.Hashtable;
 public class Tile {
 	private int sides;
 	private Hashtable<Integer,Tile> neighbors;
-	private Animal animal;
+	protected Animal animal;
 	private Thing thing;
-	public boolean AcceptPanda(Panda p) {return true;}
-	public boolean AcceptOrangutan(Orangutan o) {return true;}
-	public void RemoveAnimal() {}
+	public boolean AcceptPanda(Panda p) {
+		if (animal!=null) {
+			animal.CollideWithPanda(p);
+			return false;
+		}
+		return true;
+	}
+	public boolean AcceptOrangutan(Orangutan o) {
+		if (animal!=null){
+			if (animal.CollideWithOrangutan(o)==true) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
+	public void RemoveAnimal() {
+		this.animal=null;
+	}
 	public void JumpedOn() {}
-	public Tile GetNeighbor(int side) {return null;}
-	public int GetSides() {return sides;}
-	public int CompareTile(Tile t) {return 0;}
-	public void SetAnimal (Animal a) {animal=a;}
+	public Tile GetNeighbor(int side) {
+		return neighbors.get(side);
+	}
+	public int GetSides() {
+		return sides;
+	}
+	public int CompareTile(Tile t) {return 0;}  //nincs kész
+	public void SetAnimal (Animal a) {
+		animal=a;
+	}
 }
