63dd110452b908f6fda0685db68279815faa6b5b
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-21 20:07:37 +0100

    Update WeakTile.java

--- src/WeakTile.java
+++ src/WeakTile.java
@@ -2,9 +2,36 @@
 public class WeakTile extends Tile {
 	private int steps;
 	private boolean broken;
-	public void Damage (int n) {}
-	public void Break() {}
-	public boolean AcceptPanda(Panda p) {return true;}
-	public boolean AcceptOrangutan(Orangutan o) {return true;}
-	public void JumpedOn() {}
+	public void Damage (int n) {
+		steps+=n;
+		if (steps>=20) {
+			Break();
+		}
+	}
+	public void Break() {
+		broken=true;
+	}
+	public boolean AcceptPanda(Panda p) {
+		if (animal!=null) {
+			animal.CollideWithPanda(p);
+			return false;
+		}
+		Damage(1);
+		return true;
+	}
+	public boolean AcceptOrangutan(Orangutan o) {
+		if (animal!=null){
+			if (animal.CollideWithOrangutan(o)==true) {
+				Damage(1);
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		return true;
+	}
+	public void JumpedOn() {
+		Damage(5);
+	}
 }
