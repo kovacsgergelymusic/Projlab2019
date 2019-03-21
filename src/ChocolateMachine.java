fda8fea9e0d3760879942751d18eada2140b0af9
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2019-03-21 14:31:53 +0100

    Update ChocolateMachine.java

--- src/ChocolateMachine.java
+++ src/ChocolateMachine.java
@@ -1,7 +1,20 @@
-
-public class ChocolateMachine {
-	public void Whistle() {}
-	public void Step() {}
-	public void Notify() {}
-	public void Detach(Observer o) {}
+import java.util.Random;
+public class ChocolateMachine extends Thing{
+	public void Whistle() {
+		Notify();
+	}
+	public void Step() {
+		Random rand=new Random();
+		if (rand.nextInt(1)<2) { //most még mindig csinálja
+			Whistle();
+		}
+	}
+	public void Notify() {
+		for (Observer o:observers) {
+			o.Update(this);
+		}
+	}
+	public void Detach(Observer o) {
+		observers.remove(o);
+	}
 }
