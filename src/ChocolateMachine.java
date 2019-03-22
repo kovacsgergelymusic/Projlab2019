1d3f5d28ff9a7b921ed8153a002d76274557299e
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-22 22:29:12 +0100

    Update ChocolateMachine.java

--- src/ChocolateMachine.java
+++ src/ChocolateMachine.java
@@ -1,20 +1,25 @@
 import java.util.Random;
 public class ChocolateMachine extends Thing{
 	public void Whistle() {
+		System.out.println("Whistle()");
 		Notify();
 	}
-	public void Step() {
-		Random rand=new Random();
-		if (rand.nextInt(1)<2) { //most még mindig csinálja
+	public void Step(int rand) { //tesztelés alatt nem randomozunk
+		System.out.println("Step()");
+		if (rand<2) { //most még mindig csinálja
 			Whistle();
 		}
 	}
-	public void Notify() {
+	
+	public void Notify(ArrayList<Observer> observers) {
+		System.out.println("Notify()");
 		for (Observer o:observers) {
 			o.Update(this);
 		}
 	}
-	public void Detach(Observer o) {
+	public void Detach(Observer o, ArrayList<Observer> observers) {
+		System.out.println("Detach(Observer o)");
 		observers.remove(o);
 	}
+	
 }
