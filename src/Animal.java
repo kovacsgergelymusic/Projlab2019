5b3754ab513efe0150b6e97a07724d81d555c3d8
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-22 22:19:24 +0100

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,36 +1,42 @@
 public abstract class Animal {
-	protected Panda pulled;
-	protected Tile tile;
 	
 	public void Fall() {
+		System.out.println("Fall()");
 		Destroy();
 	}
-	public void Destroy(){
+	public void Destroy(Tile tile, Game game, Panda pulled){
+		System.out.println("Destroy()");
 		tile.RemoveAnimal();
 		Game.getInstance().DeleteAnimal(this);
 		if (pulled!=null) {
-			Disband();
+			Disband(pulled);
 		}
 	}
 	public void Move(int side){
-		
+		//TODO
+		System.out.println("Move(int side)");
 	}
 	public boolean CollideWithPanda(Panda p){
+		System.out.println("CollideWithPanda(Panda p)");
 		return false;
 		}
 	public boolean CollideWithOrangutan(Orangutan o){
+		System.out.println("CollideWithOrangutan(Orangutan o)");
 		return false;
 		}
-	public void Disband(){
-		pulled.Disband();
+	public void Disband(Panda pulled, Panda p2){
+		System.out.println("Disband()");
+		pulled.Disband(p2);
 	}
-	public Tile GetTile(){
+	public Tile GetTile(Tile tile){
+		System.out.println("GetTile()");
 		return tile;
 		}
-	public Panda GetPulled(){
+	public Panda GetPulled(Panda pulled){
+		System.out.println("GetPulled()");
 		return pulled;
 		}
 	public void SetPulled(Panda p){
-		pulled=p;
+		System.out.println("SetPulled(Panda p)");
 		}
 }
