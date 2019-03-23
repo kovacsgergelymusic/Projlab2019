963d1466fa181ea570749cad91a232d3462a0f4f
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-23 02:55:31 +0100

    Update TiredPanda.java

--- src/TiredPanda.java
+++ src/TiredPanda.java
@@ -1,8 +1,14 @@
 
 public class TiredPanda extends Panda {
 	private boolean tired;
-	public void Sit() {}
-	public void Step() {}
-	public void Update(Observable ob) {}
+	public void Sit(boolean tired) {
+		System.out.println("Sit()");
+	}
+	public void Step() {
+		System.out.println("Step()");
+	}
+	public void Update(Observable ob) {
+		System.out.println("Update(Observable ob");
+	}
 
 }
