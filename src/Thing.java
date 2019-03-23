c4a4db4adab79a19546e4d27f26b2a64c6b876b1
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-23 02:33:25 +0100

    Update Thing.java

--- src/Thing.java
+++ src/Thing.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 public abstract class Thing implements Steppable, Observable{
-	private Tile tile;
-	protected ArrayList<Observer> observers;
-	public Tile GetTile() {
+
+	public Tile GetTile(Tile tile) {
+		System.out.println("GetTile()");
 		return tile;
 	}
 }
