275b1f0fa8912bdb8116a40a6f647b7c659eb3ad
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-23 19:03:36 +0100

    Update Exit.java

--- src/Exit.java
+++ src/Exit.java
@@ -1,10 +1,33 @@
 
-public class Exit extends Tile{
+public class Exit extends Tile{//TODO
 	
-	public boolean AcceptPanda(Panda p) {
+	public boolean AcceptPanda(Panda p, Tile tile) {
 		System.out.println("AcceptPanda(p)");
-		return true;}
-	public boolean AcceptOrangutan(Orangutan o) {
+		tile.RemoveAnimal();
+		p.Destroy();
+		return false;
+	}
+	public boolean AcceptOrangutan(Orangutan o, Tile entrance, Tile tile, Panda p1, Panda p2, int testcase) {
 		System.out.println("AcceptOrangutan o)");
-		return true;}
+		tile.RemoveAnimal();
+		entrance.SetAnimal(o);
+		Panda temp1 = o.GetPulled(p1);
+		Panda temp2;
+		Panda temp3;
+		//while cikklus helyett
+		if (testcase > 1){
+			if (temp != null){
+				o.AddPoint(1);
+			}
+			if (testcase ==3){
+				temp2 = temp.GetPulled(p2);
+				temp1.Destroy();
+				if (temp2 != null) o.AddPoint(1);
+				temp2.Destroy();
+				temp3 = o.GetPulled(null);
+				temp3.Destroy();
+			}
+		}
+		return false;
+	}
 }
