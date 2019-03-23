cb81c166b4d51d1b30e4894272928ad7d4216383
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-23 02:56:52 +0100

    Update Wardrobe.java

--- src/Wardrobe.java
+++ src/Wardrobe.java
@@ -1,6 +1,10 @@
 
 public class Wardrobe extends Tile {
-	private Wardrobe tile;
-	public boolean AcceptPanda(Panda p) {return true;}
-	public boolean AcceptOrangutan(Orangutan o) {return true;}
+	
+	public boolean AcceptPanda(Panda p, Wardrobe tile) {
+		System.out.println("AcceptPanda(Panda p)");
+		return true;}
+	public boolean AcceptOrangutan(Orangutan o, Wardrobe tile) {
+		System.out.println("AcceptOrangutan(Orangutan o)");
+		return true;}
 }
