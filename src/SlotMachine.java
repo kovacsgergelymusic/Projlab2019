365ddcfb9d8bcbb3426ad6f2aec7c65138b4be91
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-23 02:32:14 +0100

    Update SlotMachine.java

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,20 +1,24 @@
 import java.util.Random;
 public class SlotMachine extends Thing {
 	public void Jingle() {
+		System.out.println("Jingle()");
 		Notify();
 	}
-	public void Step() {
-		Random rand=new Random();
-		if (rand.nextInt(1)<2) {	//most még mindig csinálja
+	public void Step(int random) {
+		System.out.println("Step()");
+		
+		if (random<2) {	//most még mindig csinálja
 			Jingle();
 		}
 	}
-	public void Notify() {
+	public void Notify(ArrayList<Observer> observers) {
+		System.out.println("Notify()");
 		for (Observer o:observers) {
 			o.Update(this);
 		}
 	}
-	public void Detach(Observer o) {
+	public void Detach(Observer o, ArrayList<Observer> observers) {
+		System.out.println("Detach(Observer o)");
 		observers.remove(o);
 	}
 }
