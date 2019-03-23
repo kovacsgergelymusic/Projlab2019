043885475bae34308bef850bf27434a94af961b9
uckingleaf <41126408+uckingleaf@users.noreply.github.com>
2019-03-23 02:27:59 +0100

    Update Panda.java

--- src/Panda.java
+++ src/Panda.java
@@ -3,21 +3,24 @@ import java.util.Random;
 public class Panda extends Animal implements Observer, Steppable {
 	protected Animal puller;
 	private ArrayList<Observable> observables;
-	public void Move(int side) {}
+	public void Move(int side) {}//Animal move
 	
-	public boolean CollideWithPanda(Panda p){		
+	public boolean CollideWithPanda(Panda p){
+		System.out.println("CollideWithPanda(Panda p)");
 		return false;
 	}
-	public boolean CollideWithOrangutan(Orangutan o){
+	public boolean CollideWithOrangutan(Orangutan o, Animal puller){
+		System.out.println("CollideWithOrangutan(Orangutan o)");
 		if (puller==null){
 			CaughtbyOrangutan(o);
 			return true;
 			}
 		return false;
-		}
-	public  void CaughtbyOrangutan(Orangutan o){	//nincs kész
-		Tile oran_tile = o.GetTile();
-                Panda lead_panda = o.GetPulled();
+	}
+	public  void CaughtbyOrangutan(Orangutan o, Tile tile, Tile oran_tile, Panda lead_panda){ //nincs kész
+		System.out.println("CaughtbyOrangutan(Orangutan o)");
+		oran_tile = o.GetTile(oran_tile);
+                Panda lead_panda = o.GetPulled(lead_panda);
                 o.SetPulled(this);
                 if (lead_panda != null)
                         lead_panda.SetPuller(this);
@@ -27,31 +30,35 @@ public class Panda extends Animal implements Observer, Steppable {
                 oran_tile.setAnimal(this);
                 return false;
 	}
-	public Animal GetPuller(){
+	public Animal GetPuller(Animal puller){
+		System.out.println("GetPuller()");
 		return puller;
 	}
 	public void SetPuller(Animal a){
-		puller=a;
+		System.out.println("SetPuller(Animal a)");
+		
 	}
-	public void Destroy(){
+	public void Destroy(ArrayList<Observable> observables){
+		System.out.println("Destroy()");
 		for (Observable o:observables){
 			o.Detach(this);
 			Game.getInstance().GetTimer().RemoveSteppable(this);
 			Game.getInstance().DeleteAnimal(this);
 		}
 	}
-	public void Step() {
-		Random rand=new Random();
+	public void Step(int random, Animal puller) {
+		System.out.println("Step()");
+		
 		if (puller==null) {
-			int sides=tile.GetSides();
-			Move(rand.nextInt(sides));
+			Move(random);
 		}
 	}
 	public void GetScared(){}
 	public void Jump(){}
 	public void Sit(){}
 	public void Update(Observable ob) {}
-	public void Disband(){
+	public void Disband(Animal puller, Panda pulled){
+		System.out.println("Disband()");
 		puller.SetPulled(null);
 		puller=null;
 		if (pulled!=null) {
